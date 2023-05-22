8208140202eb52240d3d29879b75f21828935d83
Warren <5959690+wrn14897@users.noreply.github.com>
2023-05-22 02:44:01 -0700

    feat: setup timeout and https agent (axios instance)

--- packages/node-logger/src/_logger/axiosInstance.js
+++ packages/node-logger/src/_logger/axiosInstance.js
@@ -1,5 +1,10 @@
+const https = require('https');
+
 const axios = require('axios');
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({ keepAlive: true }),
+  timeout: 30000,
+});
 
 module.exports = axiosInstance;
