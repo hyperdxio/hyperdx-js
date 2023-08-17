339b13adf3a0edd65998e1f8f54f1de86d76eeec
Warren <5959690+wrn14897@users.noreply.github.com>
2023-08-16 18:24:53 -0700

    style: cleanups

--- packages/node-opentelemetry/src/spanProcessor.ts
+++ packages/node-opentelemetry/src/spanProcessor.ts
@@ -1,5 +1,6 @@
-import { Attributes, Context, trace } from '@opentelemetry/api';
+import { Attributes, Context, context, trace } from '@opentelemetry/api';
 import { BatchSpanProcessor, Span } from '@opentelemetry/sdk-trace-base';
+import { suppressTracing } from '@opentelemetry/core';
 
 import hdx from './debug';
 
@@ -7,47 +8,74 @@ const HDX_CONTEXT_REFRESHER_INTERVAL = 10000;
 const HDX_CONTEXT_TRACE_ATTRIBUTES_EXPIRATION = 5 * 60 * 1000;
 
 class HyperDXContext {
-  private readonly _traceAttributes = new Map<
+  private readonly _traceAttributes = new Map<string, Attributes>();
+  private readonly _traceMap = new Map<
     string,
     {
-      attributes: Attributes;
-      lastReadAt: number;
+      spans: Span[];
+      lastUpdateAt: number;
     }
   >();
 
   constructor() {
     // expires after 5 minutes
     setInterval(() => {
-      hdx('Clearing _traceAttributes...');
+      hdx(`Running _traceMap expiration check`);
       const now = Date.now();
-      for (const [traceId, data] of this._traceAttributes.entries()) {
-        if (now - data.lastReadAt > HDX_CONTEXT_TRACE_ATTRIBUTES_EXPIRATION) {
+      for (const [traceId, data] of this._traceMap.entries()) {
+        if (now - data.lastUpdateAt > HDX_CONTEXT_TRACE_ATTRIBUTES_EXPIRATION) {
+          this._traceMap.delete(traceId);
           this._traceAttributes.delete(traceId);
-          hdx(`Deleted traceId ${traceId} from _traceAttributes`);
+          hdx(`Deleted traceId ${traceId} from _traceMap and _traceAttributes`);
         }
       }
     }, HDX_CONTEXT_REFRESHER_INTERVAL);
   }
 
-  setTraceAttributes = (attributes: Attributes): void => {
-    const currentSpan = trace.getActiveSpan();
-    if (!currentSpan) {
-      return;
+  _getActiveSpanTraceId = (): string | undefined => {
+    const activeSpan = trace.getSpan(context.active());
+    if (!activeSpan) {
+      return undefined;
     }
-    const traceId = currentSpan.spanContext().traceId;
-    this._traceAttributes.set(traceId, {
-      attributes,
-      lastReadAt: Date.now(),
-    });
+    return activeSpan.spanContext().traceId;
   };
 
-  getTraceAttributes = (traceId: string): Attributes | undefined => {
-    const traceAttributes = this._traceAttributes.get(traceId);
-    if (traceAttributes) {
-      traceAttributes.lastReadAt = Date.now();
-      this._traceAttributes.set(traceId, traceAttributes);
+  addTraceSpan = (traceId: string, span: Span): void => {
+    const traceData = this._traceMap.get(traceId);
+    if (!traceData) {
+      this._traceMap.set(traceId, {
+        spans: [span],
+        lastUpdateAt: Date.now(),
+      });
+    } else {
+      traceData.spans.push(span);
+      traceData.lastUpdateAt = Date.now();
     }
-    return traceAttributes?.attributes;
+    this.setTraceAttributesForAllSpans(traceId);
+  };
+
+  setTraceAttributes = (attributes: Attributes): void => {
+    const currentActiveSpanTraceId = this._getActiveSpanTraceId();
+    if (!currentActiveSpanTraceId) {
+      return;
+    }
+    this._traceAttributes.set(currentActiveSpanTraceId, attributes);
+    this.setTraceAttributesForAllSpans(currentActiveSpanTraceId);
+  };
+
+  setTraceAttributesForAllSpans = (traceId: string): void => {
+    const attributes = this._traceAttributes.get(traceId);
+    if (!attributes) {
+      return;
+    }
+    const traceData = this._traceMap.get(traceId);
+    if (traceData) {
+      for (const span of traceData.spans) {
+        if (!span.ended) {
+          span.setAttributes(attributes);
+        }
+      }
+    }
   };
 }
 
@@ -55,10 +83,11 @@ export const hyperDXContext = new HyperDXContext();
 
 export default class HyperDXSpanProcessor extends BatchSpanProcessor {
   onStart(_span: Span, _parentContext: Context): void {
-    const traceId = _span.spanContext().traceId;
-    const traceAttributes = hyperDXContext.getTraceAttributes(traceId);
-    if (traceAttributes) {
-      _span.setAttributes(traceAttributes);
-    }
+    // prevent downstream exporter calls from generating spans
+    context.with(suppressTracing(context.active()), () => {
+      const traceId = _span.spanContext().traceId;
+      hdx(`Adding traceId ${traceId} to _traceMap`);
+      hyperDXContext.addTraceSpan(traceId, _span);
+    });
   }
 }
