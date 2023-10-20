45301c5017738e053e562bb22bf93464b9bafaa2
Warren <5959690+wrn14897@users.noreply.github.com>
2023-10-20 11:13:46 -0700

    fix: address protobufjs eval issue (#57)
    * fix: protobufjs eval issue
    
    * docs: add changeset

--- packages/browser/rollup.config.ts
+++ packages/browser/rollup.config.ts
@@ -1,8 +1,9 @@
-import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import replace from 'rollup-plugin-re';
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
-import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default {
@@ -15,6 +16,16 @@ export default {
     },
   ],
   plugins: [
+    // FIXME: a patch to the unsafe-eval issue
+    // https://github.com/protobufjs/protobuf.js/issues/593
+    replace({
+      patterns: [
+        {
+          test: /eval.*\(moduleName\);/g,
+          replace: 'undefined;',
+        },
+      ],
+    }),
     json(),
     typescript(),
     commonjs(),
