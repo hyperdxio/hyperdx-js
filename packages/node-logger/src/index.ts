2a69810c4070e2b115adbbdb4c6239f6cc30017a
Sacha Weatherstone <sachaw100@hotmail.com>
2024-01-04 16:49:14 +1000

    Export Logger class (#76)
    * Export Logger class
    
    * rename export, add changeset

--- packages/node-logger/src/index.ts
+++ packages/node-logger/src/index.ts
@@ -1,8 +1,9 @@
 import HyperDXWinston from './winston';
 import { HyperDXNestLoggerModule } from './nest';
+import { Logger as HyperDXLogger } from './logger';
 
 import type { HyperDXNestLogger as HyperDXNestLoggerT } from './nest';
 
 export type HyperDXNestLogger = HyperDXNestLoggerT;
 
-export { HyperDXWinston, HyperDXNestLoggerModule };
+export { HyperDXWinston, HyperDXNestLoggerModule, HyperDXLogger };
