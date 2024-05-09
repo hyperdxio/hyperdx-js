1013f860ee38b4ca18ffdca6bc4ed16420f09194
Warren <5959690+wrn14897@users.noreply.github.com>
2024-05-09 12:21:03 -0700

    style: cleanup tracing.ts

--- packages/node-opentelemetry/src/tracing.ts
+++ packages/node-opentelemetry/src/tracing.ts
@@ -1,16 +1,3 @@
-import {
-  DEFAULT_HDX_NODE_ADVANCED_NETWORK_CAPTURE,
-  DEFAULT_HDX_NODE_BETA_MODE,
-  DEFAULT_HDX_NODE_CONSOLE_CAPTURE,
-  DEFAULT_HDX_NODE_STOP_ON_TERMINATION_SIGNALS,
-} from './constants';
 import { initSDK } from './otel';
 
-// TODO: Support more instrumentation configuration options
-
-initSDK({
-  betaMode: DEFAULT_HDX_NODE_BETA_MODE,
-  consoleCapture: DEFAULT_HDX_NODE_CONSOLE_CAPTURE,
-  advancedNetworkCapture: DEFAULT_HDX_NODE_ADVANCED_NETWORK_CAPTURE,
-  stopOnTerminationSignals: DEFAULT_HDX_NODE_STOP_ON_TERMINATION_SIGNALS,
-});
+initSDK({});
