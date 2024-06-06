7142fa8cae768de6c2ac402ffb5b92a050c6f5f2
Warren <5959690+wrn14897@users.noreply.github.com>
2024-06-05 21:23:10 -0700

    style: rename fileName to fullName

--- packages/cli/src/lib.ts
+++ packages/cli/src/lib.ts
@@ -64,7 +64,7 @@ export const uploadSourcemaps = async ({
 
   const uploadKeys = fileList.map(({ name }) => ({
     basePath: basePath || '',
-    fileName: name,
+    fullName: name,
     releaseId,
   }));
 
