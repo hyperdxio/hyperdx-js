5ca35ee7d59cd3af73a79fd7276d8060b35a7be4
Warren <5959690+wrn14897@users.noreply.github.com>
2024-06-06 23:14:18 -0700

    feat: introduce @hyperdx/cli pkg (#131)
    * feat: setup project
    
    * docs: add changeset
    
    * feat: support HYPERDX_API_ACCESS_KEY
    
    * dx: -k should be optional
    
    * feat: update .npmignore
    
    * feat: setup prebuild
    
    * fix: update publish config
    
    * feat: compute s3 key on the server side
    
    * style: rename fileName to fullName
    
    * feat: send pkg version
    
    * chore: bump

---
+++ packages/cli/README.md
@@ -0,0 +1,32 @@
+# Command Line Interface for HyperDX
+
+## Uploading source maps to HyperDX
+
+In your build pipeline, you will need to run the CLI tool. Here's how to run it:
+
+```sh
+npx @hyperdx/cli upload-sourcemaps --path="/path/to/sourcemaps" --apiKey="your-api-key"
+```
+
+You can also add this as an npm script
+
+```json
+// In package.json
+
+{
+  "scripts": {
+    "upload-sourcemaps": "npx @hyperdx/cli upload-sourcemaps --path=\"/path/to/sourcemaps\""
+  }
+}
+```
+
+Optionally, you can set the `HYPERDX_API_ACCESS_KEY` environment variable to avoid passing the `apiKey` flag.
+
+## Contributing
+
+You can test your changes locally by running the following commands:
+
+```sh
+yarn build
+node dist/index.js upload ...
+```
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import { uploadSourcemaps } from './lib.js';
+
+program.name('@hyperdx/cli').description('HyperDX Command Line Interface');
+
+program
+  .command('upload-sourcemaps')
+  .option('-k, --apiKey <string>', 'The HyperDX personal api access key')
+  .option(
+    '-u, --apiUrl [string]',
+    'An optional api url for self-hosted deployments',
+  )
+  .option(
+    '-rid, --releaseId [string]',
+    'An optional release id to associate the sourcemaps with',
+  )
+  .option(
+    '-p, --path [string]',
+    'Sets the directory of where the sourcemaps are',
+    '.',
+  )
+  .option(
+    '-bp, --basePath [string]',
+    'An optional base path for the uploaded sourcemaps',
+  )
+  .action(uploadSourcemaps);
+
+program.parse();
