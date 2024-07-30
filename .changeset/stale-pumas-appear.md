26ae2d9c11182981fa75c5b866da2e00982988d6
Warren <5959690+wrn14897@users.noreply.github.com>
2024-07-29 17:08:00 -0700

    feat: support `OTEL_METRIC_EXPORT_INTERVAL` and `OTEL_METRIC_EXPORT_TIMEOUT`  (#168)
    ref: https://github.com/open-telemetry/opentelemetry-js/issues/4655

---
+++ .changeset/stale-pumas-appear.md
@@ -0,0 +1,5 @@
+---
+'@hyperdx/node-opentelemetry': patch
+---
+
+feat: support OTEL_METRIC_EXPORT_INTERVAL and OTEL_METRIC_EXPORT_TIMEOUT
--- packages/node-opentelemetry/src/metrics.ts
+++ packages/node-opentelemetry/src/metrics.ts
@@ -1,12 +1,17 @@
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 
-import { DEFAULT_OTEL_METRICS_EXPORTER_URL } from './constants';
+import {
+  DEFAULT_OTEL_METRIC_EXPORT_INTERVAL,
+  DEFAULT_OTEL_METRIC_EXPORT_TIMEOUT,
+  DEFAULT_OTEL_METRICS_EXPORTER_URL,
+} from './constants';
 
 export const getHyperDXMetricReader = () =>
   new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter({
       url: DEFAULT_OTEL_METRICS_EXPORTER_URL,
     }),
-    exportIntervalMillis: 1000,
+    exportIntervalMillis: DEFAULT_OTEL_METRIC_EXPORT_INTERVAL,
+    exportTimeoutMillis: DEFAULT_OTEL_METRIC_EXPORT_TIMEOUT,
   });
